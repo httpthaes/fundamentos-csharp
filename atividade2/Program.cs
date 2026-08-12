bd3ab9153f5764828710e9201eab328be1efd06f
Thais <thaiserica1304@gmail.com>
2026-08-12 18:55:59 -0300

    add exercicio 2

---
+++ atividade2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Digite um número inteiro:");
+
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Antecessor: {numero - 1}");
+Console.WriteLine($"Número digitado: {numero}");
+Console.WriteLine($"Sucessor: {numero + 1}");
