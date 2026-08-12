32464b0432e68c1d1a5aca1c308bc5ef62e6a496
Thais <thaiserica1304@gmail.com>
2026-08-12 18:45:14 -0300

    add exercicio 1

---
+++ atividade1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Digite o primeiro número:"); 
+
+int primeiroNumero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número:");
+int segundoNumero = int.Parse(Console.ReadLine());
+
+int soma = primeiroNumero + segundoNumero;
+Console.WriteLine($"A soma é {soma}");
