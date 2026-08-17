f994563251ed4b7bcd9c046810ec875782c315df
Thais <thaiserica1304@gmail.com>
2026-08-17 18:16:12 -0300

    exercicio 3 - área de um retângulo

---
+++ atividade3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite a base: ");
+double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a altura: ");
+double alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+double area = baseRetangulo * alturaRetangulo;
+double perimetro = (2 * baseRetangulo) + (2 * alturaRetangulo);
+
+Console.WriteLine($"Área: {area}");
+Console.WriteLine($"Perímetro: {perimetro}");
