5f8ecb4cbb2e916719402e6138700cc204d6a61e
Thais <thaiserica1304@gmail.com>
2026-08-17 19:23:54 -0300

    add exercicio 9 - soma dos números pares

---
+++ atividade9/Program.cs
@@ -0,0 +1,13 @@
+﻿int soma = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    bool par = i % 2 == 0;
+
+    if (par)
+    {
+        soma += i;
+    }
+}
+
+Console.Write($"Soma dos números pares: {soma}");
