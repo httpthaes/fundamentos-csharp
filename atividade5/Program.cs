4345b0ea84bf7f7e5d83215168fdffb872b1e63d
Thais <thaiserica1304@gmail.com>
2026-08-17 18:49:42 -0300

    add exercicio 5 - calculadora

---
+++ atividade5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("------- CALCULADORA -------");
+
+Console.Write("Digite o primeiro número: ");
+double numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+double numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Soma: {numero1 + numero2}");
+Console.WriteLine($"Subtração: {numero1 - numero2}");
+Console.WriteLine($"Multiplicação: {numero1 * numero2}");
+Console.WriteLine($"Divisão: {numero1 / numero2}");
