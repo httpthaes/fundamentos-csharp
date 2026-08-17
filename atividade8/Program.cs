6289892a80370d393ae7ef1330efb3081da16783
Thais <thaiserica1304@gmail.com>
2026-08-17 19:09:07 -0300

    add exercicio 8 - tabuada

---
+++ atividade8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"---- TABUADA DE {numero} ----");
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} * {i} = {numero * i}");
+}
