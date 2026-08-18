f6b999469245cd1e9e79d40af39b3a4be8d81d76
Thais <thaiserica1304@gmail.com>
2026-08-18 04:57:21 -0300

    add exercicios 16 - contagem de pares

---
+++ atividade16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+List<int> pares = new List<int>();
+int contagem = 0;
+
+for (int i = 0; i < 20; i++)
+{
+    Console.Write($"{i + 1} - Digite um número: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    bool par = numero % 2 == 0;
+
+    if (pares.Contains(numero))
+    {
+        pares.Remove(numero);
+    } else if (par)
+    {
+        pares.Add(numero);
+        contagem++;
+    } 
+}
+
+Console.WriteLine($"Pares:");
+
+foreach (var par in pares)
+{
+    Console.WriteLine($"{par}");
+}
+
+Console.WriteLine($"Quantidade de pares: {contagem}");
