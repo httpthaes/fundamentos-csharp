03a3b93a707ff447702a4b33a89dbc86a579e585
Thais <thaiserica1304@gmail.com>
2026-08-18 04:10:38 -0300

    add exercicio 14 - Maior elemento

---
+++ atividade14/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] numeros = new int[10];
+int maiorNumero = 0;
+int posicao = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("Digite um número: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+    numeros[i] = numero;
+
+    if (numero > maiorNumero)
+    {
+        maiorNumero = numero;
+        posicao = i;
+    }
+}
+
+Console.WriteLine($"Maior valor: {maiorNumero}");
+Console.WriteLine($"Posição: {posicao}");
