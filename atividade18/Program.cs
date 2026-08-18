76b191090c2a6761678d49aa5a264926edd36736
Thais <thaiserica1304@gmail.com>
2026-08-18 15:50:24 -0300

    add exercicio 18 - jogo da senha

---
+++ atividade18/Program.cs
@@ -0,0 +1,19 @@
+﻿int numeroAleatorio = new Random().Next(1, 101);
+int numeroDigitado = -1;
+
+do
+{
+    Console.Write($"Digite um número: ");
+    numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+    if (numeroDigitado < numeroAleatorio)
+    {
+        Console.WriteLine($"O número aleatório é maior que {numeroDigitado}");
+    }
+    else if (numeroDigitado > numeroAleatorio)
+    {
+        Console.WriteLine($"O número aleatório é menor que {numeroDigitado}");
+    }
+} while (numeroDigitado != numeroAleatorio);
+
+Console.Write($"Parabéns, você acertou o número {numeroAleatorio}.");
