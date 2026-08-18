55c25ba7a65ad8228308fdeb07a073f5d57b2949
Thais <thaiserica1304@gmail.com>
2026-08-18 00:53:23 -0300

    add exercicio 13 - verificador de senha

---
+++ atividade13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite sua senha: ");
+string senha = Console.ReadLine();
+
+if (senha.Length >= 8)
+{
+    Console.WriteLine("A senha possui 8 ou mais caracteres");
+} else
+{
+    Console.WriteLine("A senha possui menos de 8 caracteres.");
+}
