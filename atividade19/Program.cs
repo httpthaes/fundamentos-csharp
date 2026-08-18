bc850774cbf01f344d354c47801f138929010aa4
Thais <thaiserica1304@gmail.com>
2026-08-18 16:20:47 -0300

    add exercicio 19 - caixa eletrônico

---
+++ atividade19/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] notas = { 100, 50, 20, 10, 5, 2 };
+
+Console.Write("Digite um valor para saque: ");
+int valor = Convert.ToInt32(Console.ReadLine());
+
+
+foreach (var nota in notas)
+{
+    int quantidadeDeNotas = valor / nota;
+    valor = valor % nota;
+
+    if (quantidadeDeNotas > 0)
+    {
+        string textoNota = quantidadeDeNotas == 1 ? "nota" : "notas";
+        Console.WriteLine($"{quantidadeDeNotas} {textoNota} de {nota}.");
+    }
+}
+
+if (valor == 1)
+{
+    Console.WriteLine("Sobra 1 real.");
+}
