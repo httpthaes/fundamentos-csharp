43580d09ac98b7946e3ee1330faee4d77100f539
Thais <thaiserica1304@gmail.com>
2026-08-17 21:29:01 -0300

    add exercicio 10 - login simples

---
+++ atividade10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite seu nome de usuário: ");
+string usuario = Console.ReadLine();
+
+Console.Write("Digite sua senha: ");
+string senha = Console.ReadLine();
+
+while (senha != "senha123")
+{
+    Console.WriteLine("Senha incorreta! Digite novamente.");
+    Console.Write("Digite sua senha: ");
+    senha = Console.ReadLine();
+}
+
+Console.WriteLine($"Olá, {usuario}!");
