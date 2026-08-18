2faa1878568a57dbb382d76966c30a7ee70700b8
Thais <thaiserica1304@gmail.com>
2026-08-18 04:33:22 -0300

    add exercicio 15 - média de um vetor

---
+++ atividade15/Program.cs
@@ -0,0 +1,19 @@
+﻿double[] notas = new double[8];
+double soma = 0;
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"Digite a nota {i + 1}: ");
+    double nota = Convert.ToDouble(Console.ReadLine());
+
+    notas[i] = nota;
+}
+
+foreach (var nota in notas)
+{
+    soma += nota;
+}
+
+double media = soma / 8;
+Console.WriteLine($"Soma: {soma:0.#}");
+Console.WriteLine($"Média: {media:0.#}");
