cdef1ba66fdf6d719828e6335eaf959f35b40b29
Thais <thaiserica1304@gmail.com>
2026-08-18 15:38:05 -0300

    add exercicio 17 - ordem inversa

---
+++ atividade17/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numeros = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite um número: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    numeros[numeros.Length - 1 - i] = numero;
+}
+
+foreach (var numero in numeros)
+{
+    Console.WriteLine($"{numero}");
+}
