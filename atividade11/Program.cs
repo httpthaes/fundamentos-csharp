549507cf8d9ead897be99f9e899ae1a90b77720c
Thais <thaiserica1304@gmail.com>
2026-08-17 21:38:30 -0300

    add exercicio 11 - soma até digitar zero

---
+++ atividade11/Program.cs
@@ -0,0 +1,12 @@
+﻿int soma = 0;
+int numero = 0;
+
+do
+{
+    Console.Write("Digite um número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+} while (numero != 0) ;
+
+Console.WriteLine($"Soma dos valores: {soma}");
